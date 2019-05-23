4a3c16f926aa7fd94ab1193e60db870fc4b01048
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2019-05-23 15:20:56 +0700

    base-mt

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(name='base-mt',
+      version='0.0.1',
+      description="Most fundamental Python modules for Minh-Tri Pham",
+      author=["Minh-Tri Pham"],
+      packages=find_packages(),
+      install_requires=[
+        'colorama', # for colored text
+        'Cython', # for fast speed on tiny objects
+        'dask[distributed]', # for simple multiprocessing jobs
+        'dask[dataframe]', # for reading chunks of CSV files in parallel
+      ]
+    )
+

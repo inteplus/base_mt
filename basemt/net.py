8fc26ce24fe699dbdda32af7fac23a96e810c342
Minh-Tri <minhtri.pham@winnowsolutions.com>
2019-06-24 17:13:39 +0100

    basemt version 4

--- basemt/net.py
+++ basemt/net.py
@@ -1,5 +1,7 @@
 import socket as _s
+import psutil as _p
 import getpass as _getpass
+
 
 def is_port_open(addr, port, timeout=2.0):
     '''Checks if a port is open, with timeout.'''
@@ -12,6 +14,7 @@ def is_port_open(addr, port, timeout=2.0):
     except:
         return False
 
+
 def get_hostname():
     '''Returns the machine's hostname.'''
     return _s.gethostname()
@@ -20,3 +23,21 @@ def get_hostname():
 def get_username():
     '''Returns the current username.'''
     return _getpass.getuser()
+
+
+def get_all_inet4_ipaddresses():
+    '''Returns all network INET4 interfaces' IP addresses+submasks.
+    Returns
+    -------
+        retval : dict
+            A dictionary of interface_name -> (ip_address, submask)
+    '''
+    retval1 = _p.net_if_addrs()
+    retval2 = {}
+    for k, v in retval1.items():
+        for e in v:
+            if e.family == _s.AF_INET:
+                retval2[k] = (e.address, e.netmask)
+                break
+
+    return retval2
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='basemt',
-      version='0.0.3',
+      version='0.0.4',
       description="The most fundamental Python modules for Minh-Tri Pham",
       author=["Minh-Tri Pham"],
       packages=find_packages(),

93364f9177e612f705ee6aafe768e7617aa3ca8a
Minh-Tri Pham <pmtri80@gmail.com>
2020-05-04 11:47:50 +0100

    version.py: renamed version -> VERSION

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages, find_namespace_packages
-from mt.base.version import version
+from mt.base.version import VERSION
 
 setup(name='basemt',
-      version=version,
+      version=VERSION,
       description="The most fundamental Python modules for Minh-Tri Pham",
       author=["Minh-Tri Pham"],
       packages=find_packages() + find_namespace_packages(include=['mt.*']),

e714eb684bd00a45d4df26a1ea3a8378a4e433f9
Minh-Tri Pham <pmtri80@gmail.com>
2020-05-22 12:50:59 +0100

    more bug fix

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,8 @@ extensions = [
 autoapi_dirs = ["../mt", "../basemt"]
 autoapi_python_use_implicit_namespaces = True
 
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 

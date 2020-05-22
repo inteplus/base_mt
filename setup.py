e59cccfa96e7b5eadb5cd5188de170ae4eb707c1
Minh-Tri Pham <pmtri80@gmail.com>
2020-05-22 14:15:51 +0100

    More urls

--- setup.py
+++ setup.py
@@ -12,5 +12,10 @@ setup(name='basemt',
           'colorama',  # for colored text
           'Cython',  # for fast speed on tiny objects
           'dask[distributed]',  # for simple multiprocessing jobs
-      ]
+      ],
+      url='https://github.com/inteplus/basemt',
+      project_urls={
+          'Documentation': 'https://basemt.readthedocs.io/en/stable/',
+          'Source Code': 'https://github.com/inteplus/basemt',
+          }
       )
